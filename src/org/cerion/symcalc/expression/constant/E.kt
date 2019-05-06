29c67ef9f66b7c74717a9af6cf9bc64c64f8f994
andy <andy@cerion.org>
2019-05-05 21:33:04 -0700

    added E constant

---
+++ src/org/cerion/symcalc/expression/constant/E.kt
@@ -0,0 +1,37 @@
+package org.cerion.symcalc.expression.constant
+
+import org.cerion.symcalc.Environment
+import org.cerion.symcalc.expression.ConstExpr
+import org.cerion.symcalc.expression.Expr
+import org.cerion.symcalc.expression.number.RealNum
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+class E : ConstExpr() {
+    override fun toString(): String = "E"
+
+    override fun evaluate(): Expr {
+        if (env.isNumericalEval) {
+            if (env.precision == Environment.SYSTEM_DECIMAL_PRECISION)
+                return RealNum.create(Math.E)
+
+            return RealNum.create( getEToDigits(env.precision) )
+        }
+        else
+            return this
+    }
+
+    private fun getEToDigits(n: Int): BigDecimal {
+        var e: BigDecimal = BigDecimal.ONE.setScale(n+1)
+        var next: BigDecimal = BigDecimal.ONE.setScale(n+1)
+
+        var i = 2
+        while (next.compareTo(BigDecimal.ZERO) != 0) {
+            e += next
+            next = next.divide(BigDecimal(i).setScale(n+1), RoundingMode.HALF_UP)
+            i++
+        }
+
+        return e.setScale(n, RoundingMode.HALF_UP)
+    }
+}
