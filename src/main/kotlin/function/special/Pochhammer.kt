2439bdcef1c8508601e7773c05276e36032418ea
andy <andy@cerion.org>
2020-12-20 13:35:30 -0800

    implemented Pochhammer

--- src/main/kotlin/function/special/Pochhammer.kt
+++ src/main/kotlin/function/special/Pochhammer.kt
@@ -1,12 +1,27 @@
 package org.cerion.symcalc.function.special
 
 import org.cerion.symcalc.expression.Expr
+import org.cerion.symcalc.expression.number.Integer
+import org.cerion.symcalc.expression.number.NumberExpr
 import org.cerion.symcalc.function.FunctionExpr
 
 class Pochhammer(vararg e: Expr) : FunctionExpr(*e) {
     override fun evaluate(): Expr {
-        TODO("Not yet implemented")
+        val x = get(0)
+        val n = get(1)
+
+        if (x is NumberExpr && n is Integer) {
+            if (n.isZero)
+                return Integer.ONE
+
+            var result = x
+            for (i in 1 until n.intValue()) {
+                result *= x + Integer(i)
+            }
+
+            return result
+        }
+
+        return this
     }
-
-
 }
--- src/test/java/function/special/PochhammerTest.kt
+++ src/test/java/function/special/PochhammerTest.kt
@@ -1,11 +1,34 @@
 package org.cerion.symcalc.function.special
 
+import org.cerion.symcalc.expression.number.Integer
+import org.cerion.symcalc.`==`
+import org.cerion.symcalc.assertAll
+import org.cerion.symcalc.expression.number.RealBigDec
 import org.junit.Test
 
 internal class PochhammerTest {
 
     @Test
+    fun integer() {
+        assertAll(
+                Pochhammer(Integer(0), Integer(0)) `==` Integer(1),
+                Pochhammer(Integer(0), Integer(1)) `==` Integer(0),
+                Pochhammer(Integer(1), Integer(0)) `==` Integer(1),
+                Pochhammer(Integer(1), Integer(1)) `==` Integer(1),
+                Pochhammer(Integer(2), Integer(1)) `==` Integer(2),
+                Pochhammer(Integer(2), Integer(3)) `==` Integer(24),
+                Pochhammer(Integer(3), Integer(3)) `==` Integer(60)
+        )
+    }
+
+    @Test
     fun realBigDec() {
-        // TODO current implementation uses integers as 2nd parameter
+        val x = RealBigDec("3.3333")
+        assertAll(
+                Pochhammer(x, Integer(0)) `==` Integer(1),
+                Pochhammer(x, Integer(1)) `==` x,
+                Pochhammer(x, Integer(2)) `==` RealBigDec("14.444"),
+                Pochhammer(x, Integer(5)) `==` RealBigDec("3577.8"),
+        )
     }
 }
