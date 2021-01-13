20dcb67ff058bb7ff43cea3da40c9136c909154a
Andrew Sheragy <asheragy@peak6.com>
2021-01-12 17:48:37 -0800

    Implemented EulerE

--- src/main/kotlin/function/integer/EulerE.kt
+++ src/main/kotlin/function/integer/EulerE.kt
@@ -1,9 +1,12 @@
 package org.cerion.symcalc.function.integer
 
+import org.cerion.symcalc.constant.I
 import org.cerion.symcalc.expression.Expr
+import org.cerion.symcalc.expression.ListExpr
 import org.cerion.symcalc.function.FunctionExpr
+import org.cerion.symcalc.function.arithmetic.Power
 import org.cerion.symcalc.number.Integer
-import org.nevec.rjm.Euler
+import org.cerion.symcalc.number.NumberExpr
 
 class EulerE(vararg e: Any) : FunctionExpr(*e) {
 
@@ -14,16 +17,40 @@ class EulerE(vararg e: Any) : FunctionExpr(*e) {
             if (n.isOdd)
                 return Integer.ZERO
 
-            val e = Euler()
             val t = (n / Integer(2)) as Integer
-
-            val eval = e.at(t.intValue())
-            if (t.isOdd)
-                return Integer(eval).unaryMinus()
-
-            return Integer(eval)
+            return iteratedSum(t.intValue())
         }
 
         return this
+    }
+
+    // FEAT if a series of euler numbers is needed a more efficient algorithm can get them recursively
+
+    // Double sum method of calculating E_2n
+    private fun iteratedSum(n: Int): Integer {
+        var sum: Expr = Integer.ZERO
+
+        for(k in 1..(2*n + 1) step 2) {
+
+            var innerSum: Expr = Integer.ZERO
+            val binomial = Binomial(k).eval() as ListExpr
+            for(j in 0..k) {
+                var numer: NumberExpr = if(j % 2 == 0) Integer.ONE else Integer.NEGATIVE_ONE
+                numer *= Integer(k - 2*j).pow(2*n + 1)
+                numer /= Integer.TWO.pow(k)
+                numer /= Power(I(), k).eval() as NumberExpr
+                numer /= Integer(k)
+
+                innerSum += binomial[j] * numer
+            }
+
+            //println("$k $innerSum")
+
+            sum += innerSum
+            if (k % 2 == 0)
+                sum += innerSum
+        }
+
+        return (I() * sum) as Integer
     }
 }
--- src/test/java/function/integer/EulerETest.kt
+++ src/test/java/function/integer/EulerETest.kt
@@ -1,6 +1,7 @@
 package org.cerion.symcalc.function.integer
 
 import org.cerion.symcalc.`==`
+import org.cerion.symcalc.`should equal`
 import org.junit.jupiter.api.Assertions.*
 import kotlin.test.Test
 
@@ -27,5 +28,10 @@ internal class EulerETest {
             EulerE(8) `==` 1385,
             EulerE(10) `==` -50521
         )
+    }
+
+    @Test
+    fun large() {
+        EulerE(100).eval() `should equal` "2903528346661097497054603834764435875077553006646158945080492319146997643370625023889353447129967354174648294748510553528692457632980625125"
     }
 }
