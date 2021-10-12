772ba853816aed7ae9590755a10a5a6a23111711
andy <andy@cerion.org>
2021-10-11 19:59:34 -0700

    BigDec addition

--- bignum/src/main/kotlin/BigDec.kt
+++ bignum/src/main/kotlin/BigDec.kt
@@ -12,7 +12,7 @@ class BigDec {
     }
 
     constructor(str: String) {
-        var index = str.indexOf(".")
+        val index = str.indexOf(".")
         if (index == -1)
             scale = 0
         else
@@ -21,4 +21,37 @@ class BigDec {
         value = BigInt(str.replace(".", ""))
     }
 
+    override fun toString(): String {
+        if (scale == 0)
+            return value.toString()
+
+        val result = StringBuilder(value.toString())
+        if(result.length < scale)
+            result.insert(0, "0".repeat(scale - result.length + 1))
+        result.insert(result.length - scale, ".")
+        return result.toString()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is BigDec)
+            return scale == other.scale && value == other.value
+
+        return false
+    }
+
+    operator fun plus(other: BigDec): BigDec {
+        if (scale == other.scale)
+            return BigDec(value + other.value, scale)
+
+        if (scale > other.scale)
+            return other + this
+
+        var diff = other.scale - scale
+        var pow = BigInt("10").pow(diff)
+        val scaled = this.value * pow
+        
+
+
+        return BigDec(scaled + other.value, other.scale)
+    }
 }
--- bignum/src/test/kotlin/BigDecTest.kt
+++ bignum/src/test/kotlin/BigDecTest.kt
@@ -9,20 +9,41 @@ import kotlin.test.*
 class BigDecTest {
 
     @Test
-    fun fromString() {
+    fun toFromString() {
         BigDec("10000000000.0").apply {
             assertEquals(1, scale)
             assertEquals("100000000000", value.toString())
+            assertEquals("10000000000.0", toString())
         }
 
         BigDec("10000000000").apply {
             assertEquals(0, scale)
             assertEquals("10000000000", value.toString())
+            assertEquals("10000000000", toString())
         }
 
         BigDec("0.000001").apply {
             assertEquals(6, scale)
             assertEquals("1", value.toString())
+            assertEquals("0.000001", toString())
         }
+    }
+
+    @Test
+    fun addition() {
+        val a = BigDec("10000.00")
+        val b = BigDec("100")
+        val c = BigDec("0.000001")
+
+        assertEquals(BigDec("20000.00"), a + a)
+        assertEquals(BigDec("200"), b + b)
+        assertEquals(BigDec("0.000002"), c + c)
+
+        assertEquals(BigDec("10100.00"), a + b)
+        assertEquals(BigDec("10100.00"), b + a)
+        assertEquals(BigDec("10000.000001"), a + c)
+        assertEquals(BigDec("10000.000001"), c + a)
+        assertEquals(BigDec("100.000001"), b + c)
+        assertEquals(BigDec("100.000001"), c + b)
     }
 }
