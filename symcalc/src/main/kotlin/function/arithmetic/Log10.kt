943a39026b75b5299adacebe2eca7315af6d2601
Andrew Sheragy <andy@cerion.org>
2022-04-23 21:59:46 -0700

    added log10 RealBigDec

--- symcalc/src/main/kotlin/function/arithmetic/Log10.kt
+++ symcalc/src/main/kotlin/function/arithmetic/Log10.kt
@@ -2,6 +2,8 @@ package org.cerion.symcalc.function.arithmetic
 
 import org.cerion.symcalc.expression.Expr
 import org.cerion.symcalc.function.FunctionExpr
+import org.cerion.symcalc.number.Integer
+import org.cerion.symcalc.number.RealBigDec
 import org.cerion.symcalc.number.RealDouble
 import kotlin.math.log10
 
@@ -12,6 +14,9 @@ class Log10(vararg e: Any) : FunctionExpr(*e) {
 
         if (n is RealDouble)
             return RealDouble(log10(n.value))
+
+        if (n is RealBigDec)
+            return Log(n) / Log(Integer(10))
 
         return this
     }
--- symcalc/src/test/java/function/arithmetic/Log10Test.kt
+++ symcalc/src/test/java/function/arithmetic/Log10Test.kt
@@ -1,6 +1,8 @@
 package org.cerion.symcalc.function.arithmetic
 
+import org.cerion.symcalc.number.RealBigDec
 import org.cerion.symcalc.number.RealDouble
+import org.cerion.symcalc.`should equal`
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.*
 
@@ -10,5 +12,10 @@ internal class Log10Test {
     fun basic() {
         assertEquals(RealDouble(2.0), Log10(RealDouble(100.0)).eval())
         assertEquals(RealDouble(1.6989700043360187), Log10(RealDouble(50.0)).eval())
+    }
+
+    @Test
+    fun bigDec() {
+        Log10(RealBigDec("20", 50)).eval() `should equal` "1.3010299956639811952137388947244930267681898814621"
     }
 }
