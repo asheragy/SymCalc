7b4064f4e674cf99bb738cf034d0ea536d2c22e7
Andrew Sheragy <andy@cerion.org>
2022-12-10 10:33:19 -0800

    test cases for D

--- symcalc/src/test/java/function/calculus/DTest.kt
+++ symcalc/src/test/java/function/calculus/DTest.kt
@@ -1,8 +1,9 @@
 package org.cerion.symcalc.function.calculus
 
-import org.cerion.symcalc.expression.Expr
-import org.cerion.symcalc.expression.VarExpr
+import org.cerion.symcalc.`==`
+import org.cerion.symcalc.assertAll
 import org.cerion.symcalc.constant.Pi
+import org.cerion.symcalc.expression.VarExpr
 import org.cerion.symcalc.function.arithmetic.Times
 import org.cerion.symcalc.function.trig.Cos
 import org.cerion.symcalc.function.trig.Sin
@@ -10,48 +11,102 @@ import org.cerion.symcalc.number.Complex
 import org.cerion.symcalc.number.Integer
 import org.cerion.symcalc.number.Rational
 import org.cerion.symcalc.number.RealDouble
+import org.cerion.symcalc.`should equal`
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class DTest {
 
     @Test
-    fun single() {
+    fun constant() = assertAll(
         // Numbers
-        assertEquals(Integer.ZERO, D(Integer.TWO, VarExpr("x")).eval())
-        assertEquals(Integer.ZERO, D(RealDouble(2.354), VarExpr("x")).eval())
-        assertEquals(Integer.ZERO, D(Rational(4, 6), VarExpr("x")).eval())
-        assertEquals(Integer.ZERO, D(Complex.ZERO, VarExpr("x")).eval())
+        D(Integer.TWO, VarExpr("x")) `==` Integer.ZERO,
+        D(RealDouble(2.354), VarExpr("x")) `==` Integer.ZERO,
+        D(Rational(4, 6), VarExpr("x")) `==` Integer.ZERO,
+        D(Complex.ZERO, VarExpr("x")) `==` Integer.ZERO,
 
         // Constant
-        assertEquals(Integer.ZERO, D(Pi(), VarExpr("x")).eval())
-
-        // D(x,x) = 1
-        assertEquals(Integer.ONE, D(VarExpr("x"), VarExpr("x")).eval())
+        D(Pi(), VarExpr("x")).eval() `==` Integer.ZERO,
 
         // D(x,y) = 0
-        assertEquals(Integer.ZERO, D(VarExpr("y"), VarExpr("x")).eval())
+        D(VarExpr("y"), VarExpr("x")) `==` Integer.ZERO
+    )
+
+    @Test
+    fun sumRule() {
+        // D(1 + x) = 1
+        D(VarExpr("x") + VarExpr("y") + Integer(5), VarExpr("x")).eval() `should equal` Integer.ONE
+        // D(x - 5) = 1
+        D(VarExpr("x") - Integer(5), VarExpr("x")).eval() `should equal` Integer.ONE
+
+        // Sum rule
+        // x + x = 2
+        // x + sin(x) =
+        // gamma(x) + zeta(x) = sum of derivatives
     }
 
     @Test
-    fun basic() {
-        // D(1 + x) = 1
-        assertEquals(Integer.ONE, D(VarExpr("x") + VarExpr("y") + Integer(5), VarExpr("x")).eval())
+    fun productRule() {
+        // sin*cos
+        // 5 * sin
+        // gamma * sin
+        // gamma * zeta
+    }
 
-        // D(x - 5) = 1
-        assertEquals(Integer.ONE, D(VarExpr("x") - Integer(5), VarExpr("x")).eval())
+    @Test
+    fun quotientRule() {
+        // sin / x
+    }
+
+    @Test
+    fun power() {
+        D(VarExpr("x"), VarExpr("x")).eval() `should equal` Integer.ONE
+
+        //D(Power(VarExpr("x"), Integer.TWO), VarExpr("x")).eval() `should equal` Integer.ONE
+
+        // x^n = n*x^(n-1)
+        // Check misc things with and without x in the exponent
+
+        // a^x = a^x * ln(a)
+        // e^x = e^x
+        // e^5x = ??
+    }
+
+    @Test
+    fun log() {
+        // logA(x) = 1 / (ln(a) * x)
+        // ln(x) = 1/x
+    }
+
+    @Test
+    fun chainRule() {
+        // sin(5x)
+        // sin(cos(x))
     }
 
     @Test
     fun trig() {
-        // D(Sin(x)) = Cos(x)
-        var expected: Expr = Cos(VarExpr("x"))
-        var actual = D(Sin(VarExpr("x")), VarExpr("x")).eval()
-        assertEquals(expected, actual)
+        D(Sin(VarExpr("x")), VarExpr("x")).eval() `should equal` Cos(VarExpr("x"))
+        D(Cos(VarExpr("x")), VarExpr("x")).eval() `should equal` Times(Integer(-1), Sin(VarExpr("x")))
+        // Tan(x) = sec^2(x)
+        // cot(x) = -cosec^2(x)
+        // sec(x) = sec(x)*tan(x)
+        // cosec(x) = -cosec(x)*cot(x)
 
-        // D(Cos(x)) = -Sin(x)
-        expected = Times(Integer(-1), Sin(VarExpr("x")))
-        actual = D(Cos(VarExpr("x")), VarExpr("x")).eval()
-        assertEquals(expected, actual)
+        // sin(5x) and sin(5 + x)
+    }
+
+    @Test
+    fun inverseTrig() {
+        // all 6 functions
+    }
+
+    @Test
+    fun hyperbolicTrig() {
+        // sinh(x) = cosh(x)
+        // cosh(x) = sinh(x)
+        // tanh(x) = sech^2(x)
+        // coth(x) = -cosech^2(x)
+        // sech(x) = -sech(x)*tanh(x)
+        // coseh(x) = -cosech(x)*coth(x)
     }
 }
