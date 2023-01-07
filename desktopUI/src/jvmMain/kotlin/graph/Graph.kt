e0bda1e9e3c86514d6c1ec1be18ecf116965f595
Andrew Sheragy <andy@cerion.org>
2023-01-06 21:34:16 -0800

    add auto shifting for graph origin

--- desktopUI/src/jvmMain/kotlin/graph/Graph.kt
+++ desktopUI/src/jvmMain/kotlin/graph/Graph.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PointMode
 
@@ -12,8 +13,8 @@ import androidx.compose.ui.graphics.PointMode
 fun getPoints(): List<Pair<Float, Float>> {
     return (-10..10 step 1).map {
         val x = it / 1.0
-        //val y = Math.sin(x)
-        val y = x
+        val y = Math.sin(x)
+        //val y = x
 
         Pair(x.toFloat(), y.toFloat())
     }
@@ -22,28 +23,51 @@ fun getPoints(): List<Pair<Float, Float>> {
 val chartData = ChartData(getPoints())
 
 class ChartData(val points: List<Pair<Float, Float>>) {
-    val xMin = points.map { it.first }.minOf { it }
-    val xMax = points.map { it.first }.maxOf { it }
-    val xWidth = xMax - xMin
+    // Assumes points are ordered by X values
+    val xmin = points.first().first
+    val xmax = points.last().first
+    val xwidth = xmax - xmin
 
-    val yMin = points.map { it.second }.minOf { it }
-    val yMax = points.map { it.second }.maxOf { it }
-    val yHeight = yMax - yMin
+    val ymin = points.map { it.second }.minOf { it }
+    val ymax = points.map { it.second }.maxOf { it }
+    val yheight = ymax - ymin
 
-    fun getPoints(width: Float, height: Float): List<Offset> {
-        val centerW = width / 2
-        val centerH = height / 2
-
-        // 20 is xmax - xmin
-        val x_multiplier = width / chartData.xWidth
-        val y_multiplier = height / chartData.yHeight
-
-        return chartData.points.map { Offset(it.first * x_multiplier + centerW, centerH - it.second * y_multiplier) }
+    fun getPoints(size: Size): List<Offset> {
+        return getPoints(size.width, size.height)
     }
 
-    fun getXAxisTicks(width: Float): List<Float> {
-        val x_multiplier = width / chartData.xWidth
-        val centerW = width / 2
+    private fun getPoints(width: Float, height: Float): List<Offset> {
+        val leftHalfPercentage = xmax / xwidth
+        val centerW = width - (width * leftHalfPercentage)
+        //val centerW = width / 2
+
+        val topHalfPercentage = ymax / yheight
+        val centerH = height * topHalfPercentage
+
+        // 20 is xmax - xmin
+        val x_multiplier = width / xwidth
+        val y_multiplier = height / yheight
+
+        return points.map { Offset(it.first * x_multiplier + centerW, centerH - it.second * y_multiplier) }
+    }
+
+    fun getXAxisPosition(size: Size): Float {
+        val topHalfPercentage = ymax / yheight
+        val centerH = Math.max(0f, size.height * Math.min(1f, topHalfPercentage))
+
+        return centerH
+    }
+
+    fun getYAxisPosition(size: Size): Float {
+        val leftHalfPercentage = Math.max(0f, xmax / xwidth)
+        val centerW = Math.max(0f, size.width - (size.width * leftHalfPercentage))
+
+        return centerW
+    }
+
+    fun getXAxisTicks(size: Size): List<Float> {
+        val x_multiplier = size.width / xwidth
+        val centerW = size.width / 2
 
         // TODO implies viewport of -10 to 10
         return (-10..10).map {
@@ -51,9 +75,9 @@ class ChartData(val points: List<Pair<Float, Float>>) {
         }
     }
 
-    fun getYAxisTicks(height: Float): List<Float> {
-        val multiplier = height / yHeight
-        val center = height / 2
+    fun getYAxisTicks(size: Size): List<Float> {
+        val multiplier = size.height / yheight
+        val center = size.height / 2
 
         return (-10..10).map {
             it * multiplier + center
@@ -65,45 +89,45 @@ class ChartData(val points: List<Pair<Float, Float>>) {
 @Composable
 fun MyChart() {
     Canvas(modifier = Modifier.fillMaxSize()) {
-        val canvasWidth = size.width
-        val canvasHeight = size.height
-        val centerW = canvasWidth / 2
-        val centerH = canvasHeight / 2
-
         drawPoints(
-            points = chartData.getPoints(canvasWidth, canvasHeight),
+            points = chartData.getPoints(size),
             pointMode = PointMode.Polygon,
             color = Color.Blue,
             strokeWidth = 2.0f
         )
 
-        // Draw axis
+        // X-Axis
+        val xAxisPosition = chartData.getXAxisPosition(size)
+
         drawLine(
-            start = Offset(centerW, y = 0f),
-            end = Offset(centerW, y = canvasHeight),
+            start = Offset(0f, xAxisPosition),
+            end = Offset(size.width, xAxisPosition),
             color = Color.Black,
         )
 
-        drawLine(
-            start = Offset(0f, centerH),
-            end = Offset(canvasWidth, centerH),
-            color = Color.Black,
-        )
-
-        // Draw x axis ticks
-        chartData.getXAxisTicks(canvasWidth).forEach {
+        // Ticks
+        chartData.getXAxisTicks(size).forEach {
             drawLine(
-                start = Offset(it, y = centerH - 5),
-                end = Offset(it, y = centerH),
+                start = Offset(it, y = xAxisPosition - 5),
+                end = Offset(it, y = xAxisPosition + 5),
                 color = Color.Black,
             )
         }
 
-        // Y-axis ticks
-        chartData.getYAxisTicks(canvasHeight).forEach {
+        // Y-Axis
+        val yAxisPosition = chartData.getYAxisPosition(size)
+
+        drawLine(
+            start = Offset(yAxisPosition, y = 0f),
+            end = Offset(yAxisPosition, y = size.height),
+            color = Color.Black,
+        )
+
+        // Ticks
+        chartData.getYAxisTicks(size).forEach {
             drawLine(
-                start = Offset(centerW, it),
-                end = Offset(centerW + 5, it),
+                start = Offset(yAxisPosition - 5, it),
+                end = Offset(yAxisPosition + 5, it),
                 color = Color.Black,
             )
         }
